565403290ee89113aa3b06b759d7ffa00e8aed01
Yoshe119 <yosithaind19@gmail.com>
2026-04-07 01:01:43 +0530

    final

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-//--Navigating & Animations--//
-
-//function for scroll when click Title to sections
-const navMap = {
-        "goOverview": "overview",
-        "about-title": "about",
-        "education-title": "education", 
-        "skills-title": "skill",
-        "projects-title": "project",
-        "downloads-title": "downloads",
-        "publications-title": "publications",
-        "contact-title": "contact"
-};
-
-Object.entries(navMap).forEach(([buttonId, sectionId]) => {
-        document.getElementById(buttonId).addEventListener("click",()=> goTo(sectionId));
-});
-
-
-//function for scroll from header butons to sections
-function goTo(sectionId){
-        document.getElementById(sectionId).scrollIntoView({
-                behavior:"smooth"
-        });
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+menuIcon.onclick = () => {
+navLinks.classList.toggle('active');
 }
-
-
-//--Project & Publication Section--Dropdown//
-const cards = document.querySelectorAll(".publication-card , .project-card");
-
-cards.forEach(card =>{
-card.addEventListener("click", () => {
-
-        // Close others (optional)
-        cards.forEach(c => {
-        if (c !== card) c.classList.remove("active");
-        });    
-        
-        // Toggle clicked one
-        card.classList.toggle("active");
-})
-});
